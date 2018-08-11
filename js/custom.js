ae0139fd39010b979624860e55435465d76bcc17
viplifeng <candy_li_912@hotmail.com>
2018-08-11 23:58:19 +1000

    first commit

---
+++ js/custom.js
@@ -0,0 +1,58 @@
+jQuery(document).ready(function() {
+	
+	"use strict";
+	// Your custom js code goes here.
+	
+});
+
+function showImage(index){
+	if(index == 1){
+		$('#1').fadeIn(500);
+	}
+	else if(index == 2){
+		$('#2').fadeIn(500);
+	}
+	else if(index == 3){
+		$('#3').fadeIn(500);
+	}
+	else if(index == 4){
+		$('#4').fadeIn(500);
+	}
+	else if(index == 5){
+		$('#5').fadeIn(500);
+	}
+	else if(index == 6){
+		$('#6').fadeIn(500);
+	}
+	else if(index == 7){
+		$('#7').fadeIn(500);
+	}
+	else if(index == 8){
+		$('#8').fadeIn(500);
+	}
+	else if(index == 9){
+		$('#9').fadeIn(500);
+	}
+	else if(index == 10){
+		$('#10').fadeIn(500);
+	}
+	else if(index == 11){
+		$('#11').fadeIn(500);
+	}
+	else if(index == 12){
+		$('#12').fadeIn(500);
+	}
+	else if(index == 13){
+		$('#13').fadeIn(500);
+	}
+	else if(index == 14){
+		$('#14').fadeIn(500);
+	}
+	else if(index == 15){
+		$('#15').fadeIn(500);
+	}
+}
+
+function closeImage(){
+	$('.overlay').fadeOut(300);
+}
